7ece0644c07a2672560731ce732a44b19a6f17a4
Alexito <alexitoenano@MacBook-Pro-de-Kherin.local>
2020-11-06 18:20:29 -0600

    Updates

---
+++ precache-manifest.f1ea3cfa6e26b217dfce320507079f8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be07af85e4f832d0ad21892171ec63af",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f6775333bd86e6fac485",
+    "url": "./static/css/main.c3d7cf06.chunk.css"
+  },
+  {
+    "revision": "07f57815da687df0e5cf",
+    "url": "./static/js/2.01f7d6cd.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.01f7d6cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6775333bd86e6fac485",
+    "url": "./static/js/main.f292e7d2.chunk.js"
+  },
+  {
+    "revision": "df6a0a94f3e73ad80dee",
+    "url": "./static/js/runtime-main.40c6360a.js"
+  }
+]);
